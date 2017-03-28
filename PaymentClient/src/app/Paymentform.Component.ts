bdc5eff9a9abb3a2a717e40f74749e7a0bc34ae8
Suraj <Suraj.nd4444@gmail.com>
2017-03-28 12:07:21 +0530

    salary added

--- PaymentClient/src/app/Paymentform.Component.ts
+++ PaymentClient/src/app/Paymentform.Component.ts
@@ -2,6 +2,7 @@
 import { Req } from './request';
 import { Res } from './response';
 import { CreatePaymentLogic,PaymentLogic } from './sendpayment';
+import { SalaryPayment } from './SalaryPayment';
 
 @Component({
 
@@ -18,10 +19,16 @@ export class PaymentFormComponent {
 	constructor(private PayLogic: PaymentLogic) {
 
 	}
+
+	pad(num:string, size:number): string {
+        var s = num;
+        while (s.length < size) s = "0" + s;
+        return s;
+    }
 	
-	CreateP(uid: string, agrno: string, faccount: string, taccount: string, amount: string, ptype: string): void {
+	CreateP(uid: string, agrno: string, faccount: string, taccount: string, amount: string, ptype: string, btype: string): void {
 		if(ptype == "BKT") {
-			let d: Req = new Req(uid, agrno, faccount, taccount, amount, ptype);
+			let d: Req = new Req(uid,agrno,faccount,taccount,amount,ptype,btype);			
 			this.PayLogic.Create(d).subscribe((r) => {
 				this.out = r;
 				this.result = this.out.Returtekst;
@@ -31,7 +38,9 @@ export class PaymentFormComponent {
 		}
 		else if(ptype == "BKL")
 		{
-			let d: Req = new Req(uid, agrno, faccount, taccount, amount, ptype);
+			let d: SalaryPayment = new SalaryPayment('',agrno, uid, 'DA','DK' ,'E','DK','DK',
+			'3923','','0', faccount, '0','','', '','','','','','0000', faccount,'',taccount,'', this.pad(amount.replace(",",""),15), 'DKK','','0','DABA', 'O23YVQD5IKDJK26L3443FJO5', ptype,
+			'0','0','0','0','1','0','0','Salary','0','0','0','0');
 			this.PayLogic.SalaryCreate(d).subscribe((r) => {
 				this.out = r;
 				this.result = this.out.Returtekst;
--- PaymentClient/src/app/sendpayment.ts
+++ PaymentClient/src/app/sendpayment.ts
@@ -2,18 +2,20 @@
 import { Res } from './response';
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { SalaryPayment } from './SalaryPayment';
 //import 'rxjs';
 import { Observable } from 'rxjs/Rx';
 
 export abstract class PaymentLogic {
 
 	abstract Create(Pay: Req): Observable<Res>;	
-	abstract SalaryCreate(Pay : Req) : Observable<Res>;
+	abstract SalaryCreate(Pay : SalaryPayment) : Observable<Res>;
 }
 
 @Injectable()
 export class CreatePaymentLogic extends PaymentLogic {
-
+    
+	public beneficiaryDd : boolean;
 	constructor(private _http: Http) {
 		super();
 	}
@@ -24,7 +26,7 @@ export class CreatePaymentLogic extends PaymentLogic {
 	}
 
 	Create(Pay: Req): Observable<Res> {
-        let url = "http://localhost:63977/api/DKPayment";
+        let url = "http://localhost:63977/api/AccountPaymentService";
 		let headers = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
 		let options = new RequestOptions({ headers: headers }); // Create a request option
 		return this._http
@@ -34,8 +36,17 @@ export class CreatePaymentLogic extends PaymentLogic {
 			.catch(this.handleError);
 	}
 
-	SalaryCreate(Pay: Req): Observable<Res> {
-        let url = "http://localhost:63977/api/SalaryPayment";
+	beneficiary(ptype : string) : void{
+		if(ptype = 'BKT'){
+			this.beneficiaryDd = true;
+		}
+		else{
+			this.beneficiaryDd = false;
+		}
+	} 
+
+	SalaryCreate(Pay: SalaryPayment): Observable<Res> {
+        let url = "http://localhost:63977/api/SalaryService";
 		let headers = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
 		let options = new RequestOptions({ headers: headers }); // Create a request option
 		return this._http
