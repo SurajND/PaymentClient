13356b4f9cbfb76c681d3559893b6d2d516b059d
Suraj <Suraj.nd4444@gmail.com>
2017-04-05 20:45:07 +0530

    tansfer

--- PaymentClient/src/app/TransferInternal.ts
+++ PaymentClient/src/app/TransferInternal.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-
+import { Component, EventEmitter, Output, HostListener } from '@angular/core';
+import { ListPaymentRequest } from './listrequest';
+import { ListPaymentResponse, Payment, Payments } from './listresponse';
+import { ListLogic, ListPaymentLogic } from './listpayment';
 
 @Component({
 
@@ -12,12 +14,20 @@ export class TransferInternal {
     dis:boolean = false;
 
     constructor(){
-        
+
     }
 
     Distrue(): void {		
 		  this.dis = true;
 	}
+    
+    @HostListener("dragover", ["$event"])
+    prevent(event : any){        
+        // let eventObj: MSInputMethodContext = <MSInputMethodContext> event;
+        // let target: HTMLInputElement = <HTMLInputElement> eventObj.target;        
+        event.preventDefault();
+        event.stopPropagation();    
+    }
 
 	Disfalse(): void {		
 		  this.dis = false;
